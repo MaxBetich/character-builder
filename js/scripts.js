31f967ab733cc5246763762b63e4f187c30c25a0
Max Betich <betichm@gmail.com>
2023-03-23 16:20:45 -0700

    improve output format

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,26 @@ function characterCreator(event) {
   if (race === "Human") {
     if (strength === "strong") {
       character = "fighter";
+      document.getElementById("fighter").removeAttribute("class");
     } else if (strength === "smart") {
       character = "wizard";
+      document.getElementById("wizard").removeAttribute("class");
     }
   } else if (race === "Elf") {
     if (strength === "strong") {
       character = "ranger";
+      document.getElementById("ranger").removeAttribute("class");
     } else if (strength === "smart") {
       character = "druid";
+      document.getElementById("druid").removeAttribute("class");
     }
   } else if (race === "Orc") {
     if (strength === "strong") {
       character = "barbarian";
+      document.getElementById("barbarian").removeAttribute("class");
     } else if (strength === "smart") {
       character = "shaman";
+      document.getElementById("shaman").removeAttribute("class");
     }
   }
 
